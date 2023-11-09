e956691a91947a131dcb918aabb620fb2069bb6f
pranav801 <pranavtv801@gmail.com>
2023-11-09 18:15:33 +0530

    cors updation 2

--- project/settings.py
+++ project/settings.py
@@ -201,7 +201,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://fixmyappliances.netlify.app/',
+    'https://fixmyappliances.netlify.app',
 ]
 
 # SMTP configuration
