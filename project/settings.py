0859151ec4ced8c33b41f5ca036a31c5fece6d1a
pranav801 <pranavtv801@gmail.com>
2023-11-09 18:38:19 +0530

    cors updation 3

--- project/settings.py
+++ project/settings.py
@@ -197,7 +197,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOW_ALL_ORIGINS = True
 
 # CORS_ALLOWED_ORIGINS = [
 #     'http://localhost:5173',
