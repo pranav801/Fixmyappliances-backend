31f9ee94703e0dda07da16d73ee1bd7901d15042
pranav801 <pranavtv801@gmail.com>
2023-11-17 16:05:51 +0530

    asgi.py changed

--- project/asgi.py
+++ project/asgi.py
@@ -13,18 +13,16 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 django.setup()
 
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
-from channels.routing import ProtocolTypeRouter, URLRouter
 from message.routing import websocket_urlpatterns
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
 
-django_asgi_app = get_asgi_application()
+
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
-        "websocket": (
-            (URLRouter(websocket_urlpatterns))
-        ),
+        "http": get_asgi_application(),
+        "websocket": (URLRouter(websocket_urlpatterns)),
     }
 )
